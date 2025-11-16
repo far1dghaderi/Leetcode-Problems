9caea9448dc48dc1ad7a905b9bdb954add1adb72
Farid Ghaderi <farid.51gh@gmail.com>
2025-11-16 22:16:09 +0330

    feat: new solution for length of last word

--- length-of-last-word/solution.js
+++ length-of-last-word/solution.js
@@ -3,22 +3,33 @@
  * @return {number}
  */
 //This solution is not optimal in terms of time complexity and space complexity.
+// var lengthOfLastWord = function (s) {
+//   let newString = '';
+//   let prevChar = null;
+//   let length = 0;
+//   for (const char of s.trim()) {
+//     if (prevChar === null) {
+//       prevChar = char;
+//       newString += char;
+//       length += 1;
+//     } else {
+//       if (prevChar === ' ' && char === ' ') continue;
+//       if (char === ' ') length = 0;
+//       else length += 1;
+//       prevChar = char;
+//       newString += char;
+//     }
+//   }
+//   return length;
+// };
+
+
 var lengthOfLastWord = function (s) {
-  let newString = '';
-  let prevChar = null;
   let length = 0;
-  for (const char of s.trim()) {
-    if (prevChar === null) {
-      prevChar = char;
-      newString += char;
-      length += 1;
-    } else {
-      if (prevChar === ' ' && char === ' ') continue;
-      if (char === ' ') length = 0;
-      else length += 1;
-      prevChar = char;
-      newString += char;
-    }
+  s = s.trim();
+  for (let i = s.length - 1; 0 <= i; i--) {
+    if (s[i] !== ' ') length += 1;
+    else break;
   }
   return length;
 };
